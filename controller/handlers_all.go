d1ccd5e571ae4b1057194453ec9089d417078f02
ShamanKing1337 <maxim2001va@gmail.com>
2021-03-14 16:05:31 +0300

    change resp

--- controller/handlers_all.go
+++ controller/handlers_all.go
@@ -1,4 +1,3 @@
-
 package controller
 
 import (
@@ -7,11 +6,8 @@ import (
 	"encoding/json"
 	"fmt"
 	"net/http"
-	"net/url"
 	"strings"
 )
-
-
 
 type AllCommandsImplementation interface {
 	GetAll(w http.ResponseWriter, r *http.Request)
@@ -22,24 +18,17 @@ type AllCommandsImplementation interface {
 	AppendAll(w http.ResponseWriter, r *http.Request)
 }
 
-
-
 type allCommandsImplementation struct {
 	service service.AllCommandsService
 }
 
-func NewAllCommandsImplementation(service service.AllCommandsService) AllCommandsImplementation{
+func NewAllCommandsImplementation(service service.AllCommandsService) AllCommandsImplementation {
 	return &allCommandsImplementation{service: service}
 }
 
+func (s allCommandsImplementation) SetAll(w http.ResponseWriter, r *http.Request) {
 
-
-
-
-func(s allCommandsImplementation) SetAll(w http.ResponseWriter, r *http.Request){
-
-	if remain.CheckMethod("PUT",r){
-
+	if remain.CheckMethod("PUT", r) {
 
 		var req service.RequestAll
 		err1 := json.NewDecoder(r.Body).Decode(&req)
@@ -48,81 +37,72 @@ func(s allCommandsImplementation) SetAll(w http.ResponseWriter, r *http.Request)
 			panic(err1)
 		}
 
+		resp := s.service.Set(req)
+		fmt.Fprintf(w, "Resp: %+v", resp)
 
-		s.service.Set(req)
-
-	} else{
+	} else {
 		w.WriteHeader(405)
 	}
 
 }
 
-func(s *allCommandsImplementation) SaveAll(w http.ResponseWriter, r *http.Request){
+func (s *allCommandsImplementation) SaveAll(w http.ResponseWriter, r *http.Request) {
 
 	s.service.Save()
 
 }
 
-
-
-func(s *allCommandsImplementation) AppendAll(w http.ResponseWriter, r *http.Request){
-	if remain.CheckMethod("POST",r){
+func (s *allCommandsImplementation) AppendAll(w http.ResponseWriter, r *http.Request) {
+	if remain.CheckMethod("POST", r) {
 		var req service.RequestAll
 		err1 := json.NewDecoder(r.Body).Decode(&req)
 
 		if err1 != nil {
 			panic(err1)
 		}
-		s.service.Append(req)
-	} else{
+		resp := s.service.Append(req)
+		fmt.Fprintf(w, "Resp: %+v", resp)
+	} else {
 		w.WriteHeader(405)
 	}
 }
 
-
-
-func(s *allCommandsImplementation) GetAll(w http.ResponseWriter, r *http.Request){
-	if remain.CheckMethod("GET",r){
-		fmt.Println(url.Parse(fmt.Sprint(r.URL)))
+func (s *allCommandsImplementation) GetAll(w http.ResponseWriter, r *http.Request) {
+	if remain.CheckMethod("GET", r) {
 		str := strings.Split(fmt.Sprint(r.URL), "B")
 		str1 := strings.Split(str[1], "%")
 
-
 		var res *service.Ttl = s.service.Get(str1[0])
-		if res.ValueS != "nil"{
+		if res.ValueS != "nil" {
 			fmt.Fprintf(w, "Resp: %+v", res)
 		} else {
 			fmt.Fprintf(w, "Not Exist")
 		}
-	} else{
+	} else {
 		w.WriteHeader(405)
 	}
 
 }
 
-
-func(s *allCommandsImplementation) DeleteKey(w http.ResponseWriter, r *http.Request){
-	if remain.CheckMethod("DELETE",r){
-		fmt.Println(url.Parse(fmt.Sprint(r.URL)))
+func (s *allCommandsImplementation) DeleteKey(w http.ResponseWriter, r *http.Request) {
+	if remain.CheckMethod("DELETE", r) {
 		str := strings.Split(fmt.Sprint(r.URL), "B")
 		str1 := strings.Split(str[1], "%")
 		resp := s.service.Delete(str1[0])
 
 		fmt.Fprintf(w, "Resp: %+v", resp)
-	} else{
+	} else {
 		w.WriteHeader(405)
 	}
 }
 
-
-func(s *allCommandsImplementation) GetKeys(w http.ResponseWriter, r *http.Request){
-	if remain.CheckMethod("GET",r){
+func (s *allCommandsImplementation) GetKeys(w http.ResponseWriter, r *http.Request) {
+	if remain.CheckMethod("GET", r) {
 
 		tmp := s.service.Keys()
 
 		fmt.Fprintf(w, "Resp: %+v", tmp)
-	} else{
+	} else {
 		w.WriteHeader(405)
 	}
 }
-
--- service/all_commands.go
+++ service/all_commands.go
@@ -13,27 +13,24 @@ import (
 
 type AllCommandsService interface {
 	Get(key string) *Ttl
-	Set(req RequestAll)
+	Set(req RequestAll) string
 	Delete(key string) string
 	Keys() []string
 	Check()
 	Save()
-	Append(req RequestAll)
+	Append(req RequestAll) string
 }
 
-
-type RequestAll struct{
-	Key string
+type RequestAll struct {
+	Key    string
 	ValueS string
 	ValueL []string
 	ValueD map[string]string
 
-
-	ttl int64
+	Ttl int64
 }
 
-
-type Ttl struct{
+type Ttl struct {
 	ValueD map[string]string
 	ValueL []string
 	ValueS string
@@ -41,14 +38,13 @@ type Ttl struct{
 }
 
 type allCommandsService struct {
-	data map[string]*Ttl
+	data  map[string]*Ttl
 	mutex sync.Mutex
 }
 
-
 func (s *allCommandsService) Get(key string) *Ttl {
 	s.mutex.Lock()
-	tmp,ok := s.data[key]
+	tmp, ok := s.data[key]
 	s.mutex.Unlock()
 	if ok {
 		return tmp
@@ -59,63 +55,47 @@ func (s *allCommandsService) Get(key string) *Ttl {
 
 }
 
-func (s *allCommandsService) Set(req RequestAll)  {
+func (s *allCommandsService) Set(req RequestAll) string {
 	s.mutex.Lock()
 
-
-	if req.ttl > 0{
-		s.data[req.Key] = &Ttl{ValueS: req.ValueS, ValueL: req.ValueL, ValueD: req.ValueD, ttl :time.Now().Add(time.Second * time.Duration(req.ttl)).Unix()}
-	}else{
-		s.data[req.Key] = &Ttl{ValueS: req.ValueS, ValueL: req.ValueL, ValueD: req.ValueD, ttl:-1}
+	if req.Ttl > 0 {
+		s.data[req.Key] = &Ttl{ValueS: req.ValueS, ValueL: req.ValueL, ValueD: req.ValueD, ttl: time.Now().Add(time.Second * time.Duration(req.Ttl)).Unix()}
+	} else {
+		s.data[req.Key] = &Ttl{ValueS: req.ValueS, ValueL: req.ValueL, ValueD: req.ValueD, ttl: -1}
 	}
 	s.mutex.Unlock()
+	return "OK"
 }
 
-func (s *allCommandsService) Delete(key string)  string{
+func (s *allCommandsService) Delete(key string) string {
 	s.mutex.Lock()
 	delete(s.data, key)
 	s.mutex.Unlock()
 	return "OK"
 }
 
-
-func MapConctenation(a map[string]string, b map[string]string) map[string]string{
+func MapConctenation(a map[string]string, b map[string]string) map[string]string {
 	for k := range b {
 		a[k] = b[k]
 	}
 	return a
 }
 
-
-
-
-func ListConctenation(a []string, b []string) []string{
-	for i:=0;i< len(b);i++ {
+func ListConctenation(a []string, b []string) []string {
+	for i := 0; i < len(b); i++ {
 		a = append(a, b[i])
 	}
 	return a
 }
 
-
-
-
-func (s *allCommandsService) Append(req RequestAll) {
+func (s *allCommandsService) Append(req RequestAll) string {
 	s.mutex.Lock()
 
-
-
-
-	s.data[req.Key] = &Ttl{ValueS: s.data[req.Key].ValueS, ValueL: ListConctenation(s.data[req.Key].ValueL, req.ValueL), ValueD: MapConctenation(s.data[req.Key].ValueD, req.ValueD), ttl : s.data[req.Key].ttl}
-
+	s.data[req.Key] = &Ttl{ValueS: s.data[req.Key].ValueS, ValueL: ListConctenation(s.data[req.Key].ValueL, req.ValueL), ValueD: MapConctenation(s.data[req.Key].ValueD, req.ValueD), ttl: s.data[req.Key].ttl}
 
 	s.mutex.Unlock()
-
+	return "OK"
 }
-
-
-
-
-
 
 func (s *allCommandsService) Keys() []string {
 	var tmp = []string{}
@@ -127,13 +107,11 @@ func (s *allCommandsService) Keys() []string {
 	return tmp
 }
 
+func (s *allCommandsService) Check() {
 
-
-func (s *allCommandsService)Check(){
-
-	for{
+	for {
 		for k := range s.data {
-			if(s.data[k].ttl <=  time.Now().Unix() && s.data[k].ttl != -1){
+			if s.data[k].ttl <= time.Now().Unix() && s.data[k].ttl != -1 {
 				fmt.Println("delete: ", k)
 				delete(s.data, k)
 			}
@@ -143,11 +121,7 @@ func (s *allCommandsService)Check(){
 	}
 }
 
-
-
-
-
-func (s *allCommandsService) Save()  {
+func (s *allCommandsService) Save() {
 	tmp := make(map[string]Ttl)
 	s.mutex.Lock()
 	for k := range s.data {
@@ -164,23 +138,21 @@ func (s *allCommandsService) Save()  {
 	for k := range s.data {
 		var strL string
 		var strD string
-		for i :=0; i < len(tmp[k].ValueL);i++{
-			strL = strL +  tmp[k].ValueL[i] + ", "
+		for i := 0; i < len(tmp[k].ValueL); i++ {
+			strL = strL + tmp[k].ValueL[i] + ", "
 		}
 		for j := range tmp[k].ValueD {
 			strD = strD + "\t" + j + ": " + tmp[k].ValueD[j] + "\n "
 		}
-		fo.WriteString(k + ": " + tmp[k].ValueS + "\n" + strL + "\n" + strD + "\nttl:" + strconv.FormatInt(tmp[k].ttl,10) + "\n")
+		fo.WriteString(k + ": " + tmp[k].ValueS + "\n" + strL + "\n" + strD + "\nttl:" + strconv.FormatInt(tmp[k].ttl, 10) + "\n")
 	}
 
-
 }
-
 
 func NewAllCommandsService() AllCommandsService {
 	return &allCommandsService{data: make(map[string]*Ttl)}
 }
 
 func NewMockAllCommandsService() AllCommandsService {
-	return &allCommandsService{data: map[string]*Ttl{"key1":{ValueL: []string{"value1", "dsad"}, ValueS: "llll",ttl :time.Now().Add(time.Second * 80).Unix() }, "key2":{ValueL: []string{"value2", "ds123123ad"},ttl:time.Now().Add(time.Minute * 3).Unix() }, "key3":{ValueL: []string{"value3", "dsad", "dlllllll"}, ValueD: map[string]string{"value3": "1111111", "value2" : "jjj"},ttl:time.Now().Add(time.Minute * 6).Unix() }}}
+	return &allCommandsService{data: map[string]*Ttl{"key1": {ValueL: []string{"value1", "dsad"}, ValueS: "llll", ttl: time.Now().Add(time.Second * 80).Unix()}, "key2": {ValueL: []string{"value2", "ds123123ad"}, ttl: time.Now().Add(time.Minute * 3).Unix()}, "key3": {ValueL: []string{"value3", "dsad", "dlllllll"}, ValueD: map[string]string{"value3": "1111111", "value2": "jjj"}, ttl: time.Now().Add(time.Minute * 6).Unix()}}}
 }
